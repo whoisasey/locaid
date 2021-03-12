0787d5c85e8ae2757786a3037c4b61b175ce6792
atinachang <atinachang@gmail.com>
2021-03-11 21:27:01 -0500

    home page

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React,{Fragment} from 'react'
 import {NavLink} from 'react-router-dom'
 
 const Dashboard = () => {
+	const stats = [
+		{
+			value: "14M",
+			label: "tonnes of clothing are thrown away annually"
+		},
+		{
+			value: "84%",
+			label: "of clothes ends up in landfills"
+		},
+		{
+			value: "90%",
+			label: "of clothing donations end up with textile recyclers"
+		}
+	]
 	return (
-		<div>
+		<Fragment>
 			<h1>Donate with impact.</h1>
 			<p>Donate your clothes to charities you trust.</p>
 			<NavLink to="/locaid/all" className="ui button">Find a Trusted Charity</NavLink>
-		</div>
+			<span className="iconify" data-icon="entypo-chevron-thin-down" data-inline="false"></span>
+			<h3>Does 100% of your donation go to charity?</h3>
+			<div className="ui four statistics">
+
+			{stats.map((el, index) => {
+				return (
+					<div className="statistic" key={index}>
+						<div className="value"><strong>{el.value}</strong></div>
+						<div className="label">{ el.label}</div>
+					</div>
+				)
+			})}
+			</div>
+			<p>The biggest concern with charities is transparency.
+			Learn how we’re are making sure your 
+100% of donation goes to those in need.</p>
+			<button className="ui button">Learn More</button>
+		</Fragment>
 	)
 }
 
