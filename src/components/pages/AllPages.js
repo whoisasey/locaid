9a992aabbc87de061e20eca0817dad259db5583a
atinachang <atinachang@gmail.com>
2021-03-11 21:39:08 -0500

    nav links

--- src/components/pages/AllPages.js
+++ src/components/pages/AllPages.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import DB from '../../data.json'
 
 const AllPages = () => {
-	return (
-		<div>
-			<h2>all pages</h2>
-		</div>
-	)
-}
+	const [data, getData] = useState([]);
+
+	useEffect(() => {
+		getData(DB);
+	}, []);
+
+
+		
+		console.log(data);
+		return (
+			<Fragment>
+				<h2>all pages</h2>
+			</Fragment>
+		);
+	}
+
 
 export default AllPages
